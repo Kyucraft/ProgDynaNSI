5c950138e531d8cfb1ce06c7b4b7c3c6eae2d899
Kyucraft <42reki@gmail.com>
2021-02-11 11:18:48 +0100

    create rendumonnaie.py

---
+++ rendumonnaie.py
@@ -0,0 +1,34 @@
+'''
+[1,4,6]
+V=8
+
+8/1 = 8
+
+
+'''
+#> 
+#<
+sys_mon=[]
+Valeur = int()
+
+def gen_liste_pieces(tab,prix):
+    '''
+    Input --> tab:list, prix:int
+    Output --> liste_peces:list, liste_prix:list
+
+    Prend un systeme monétaire, le prix 
+    Sort une liste avec le bon nombre de pièces permettant de faire l'algo mais aussi leurs sizes qui est de -1
+
+    '''
+    listes_pieces=[]
+    liste_prix=[]
+    for i in range(len(tab)):
+        for j in range(prix//tab):
+            listes_pieces.append(tab[i])
+    for i in range(len(listes_pieces)):
+        liste_prix.append(-1)
+    return listes_pieces, liste_prix
+
+def pieces_necessaires(tab,prix):
+    '''
+    '''
